910d06ad361344b4df98ab6290eb45fb9902313c
yizeliang <yizeliang@126.com>
2021-04-26 20:40:46 +0800

    反序列化大小写不敏感

--- moshi/src/main/java/com/squareup/moshi/JsonUtf8Reader.java
+++ moshi/src/main/java/com/squareup/moshi/JsonUtf8Reader.java
@@ -640,10 +640,9 @@ final class JsonUtf8Reader extends JsonReader {
    */
   private int findName(String name, Options options) {
     for (int i = 0, size = options.strings.length; i < size; i++) {
-      if (name.equals(options.strings[i])) {
+      if (name.equalsIgnoreCase(options.strings[i])) {
         peeked = PEEKED_NONE;
         pathNames[stackSize - 1] = name;
-
         return i;
       }
     }
