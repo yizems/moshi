ec04491719e9066f31b99150584375210accd0bd
yizeliang <yizeliang@126.com>
2021-04-27 19:15:58 +0800

    NullSafeJsonAdapter.java 反序列化失败,转为null,不闪退

--- moshi/src/main/java/com/squareup/moshi/internal/NullSafeJsonAdapter.java
+++ moshi/src/main/java/com/squareup/moshi/internal/NullSafeJsonAdapter.java
@@ -38,11 +38,12 @@ public final class NullSafeJsonAdapter<T> extends JsonAdapter<T> {
     if (reader.peek() == JsonReader.Token.NULL) {
       return reader.nextNull();
     } else {
-        try {
-            return delegate.fromJson(reader);
-        } catch (Exception e) {
-            return null;
-        }
+      try {
+        return delegate.fromJson(reader);
+      } catch (Exception e) {
+        e.printStackTrace();
+        return null;
+      }
     }
   }
 
